f0c88b3cac14ef3bd462877209673a6f2ccb12e1
susmithaseeta <susmithaseeta2@gmail.com>
2026-08-12 15:10:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.73%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+        self.front = None
+
+    def push(self, x: int) -> None:
+        if not self.s1:
+            self.front = x
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if self.s2:
+            return self.s2[-1]
+        return self.front
+
+    def empty(self) -> bool:
+        return not self.s1 and not self.s2
